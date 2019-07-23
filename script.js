3bc80b7ee4d03604c22b3c36ff88788bf516b70a
fulelgupport <chirk0ff.poker@gmail.com>
2019-07-23 16:07:54 +0300

    mobile menu

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ btnHide.addEventListener('click', function(){
     else formHide.classList.add("hidden");
 });
 
+var menuHide = document.querySelector(".nav-list");
+var menuBtn = document .querySelector(".big-logo .menu");
+var logo = document.querySelector(".min-logo")
+
+menuBtn.addEventListener('click', function(){
+    if(menuHide.classList.contains('hidden')){
+        menuHide.classList.remove("hidden");
+        menuBtn.style.top = 240 +'px';
+        logo.style.top = 223 + 'px';
+    }
+    else{
+        menuHide.classList.add("hidden");
+        menuBtn.style.top = 18 + 'px';
+        logo.style.top = -56 + 'px';
+    }
+});
